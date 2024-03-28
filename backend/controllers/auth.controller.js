1c2db1a6d3c20226fa0e1dca13d482f5a625fa0d
Akshay Sasikumar <124657795+AlphaMaveric@users.noreply.github.com>
2024-03-28 12:14:18 +0530

    Initial Project Setup and Route setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    console.log("Sign Up User");
+}
+
+export const login = (req, res) => {
+    console.log("Login User");
+}
+
+export const logout = (req, res) => {
+    console.log("Logout User");
+}
