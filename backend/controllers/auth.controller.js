27567ed37309c9a59c8b577283dfe7b08d27a7d8
Akshay Sasikumar <124657795+AlphaMaveric@users.noreply.github.com>
2024-03-29 11:27:54 +0530

    Created Signup function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,59 @@
-export const signup = (req, res) => {
-    console.log("Sign Up User");
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+
+export const signup = async(req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender }= req.body;
+
+        if(password !== confirmPassword)
+        {
+            return res.status(400).json({error: "Passwords dont match"});
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json({error: "Username already exists"});
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullname: fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePicture: gender === "male"? maleProfilePic : femaleProfilePic
+        })
+                
+        if(newUser)
+        {
+            //Generate JWT token and set cookie
+             generateTokenAndSetCookie(newUser._id, res);
+
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                profilePicture: newUser.profilePicture
+            });
+
+        } else{
+            res.status(400).json({error: "Invalid user data"});
+        }
+    }
+    catch (error) {
+        console.log("Error in sign up path", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDb from "./database/connectToMongoDb.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+dotenv.config();
 
-    res.send("Hello world!");
-});
+app.use(express.json()); 
+// app.get("/", (req, res) => {
+
+//     res.send("Hello world!");
+// });
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> console.log("A server is running"));
+app.listen(PORT, ()=> {
+    connectToMongoDb();
+    console.log("A server is running");    
+});
